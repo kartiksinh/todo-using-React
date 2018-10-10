46dfeb4e200141f689c72541d159112dcfdf3266
Kartiksinh <kartiksinh24@gmail.com>
2018-10-10 17:17:12 +0530

    Delete functionality done

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -5,12 +5,15 @@ class TodoItems extends Component {
 	super(props);
 
 	this.createTasks = this.createTasks.bind(this);
-
 	}
 
 	createTasks(item) {
 		return <li 	onClick={()=> this.delete(item.key)}
 					key={item.key}>{item.text}</li>
+	}
+
+	delete(key){
+		this.props.delete(key);
 	}
 
 	render(){
--- src/TodoList.js
+++ src/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends Component {
 			};
 
 			this.addItem = this.addItem.bind(this);
+			this.deleteItem = this.deleteItem.bind(this);
 	 }
 	addItem(e){
 			if(this._inputElement !== ""){
@@ -31,6 +32,16 @@ class TodoList extends Component {
 			e.preventDefault();
 	}
 
+	deleteItem(key){
+		var filteredItems = this.state.items.filter(function(item) {
+			return(item.key !== key)
+		});
+
+		this.setState({
+			items: filteredItems
+		});
+	}
+
 	render(){
 		return(
 			<div className="todoListMain">
@@ -42,7 +53,8 @@ class TodoList extends Component {
 						<button type="submit">add</button>
 					</form>
 				</div>
-				<TodoItems entries={this.state.items}/>
+				<TodoItems entries={this.state.items}
+									 delete={this.deleteItem}/>
 			</div>
 		);
 	}
